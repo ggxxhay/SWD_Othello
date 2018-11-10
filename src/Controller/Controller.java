fe745f3c1ba515fdae45b26a8ab0add5742e25d7
Mark Allen <>
2018-11-11 02:31:57 +0700

    add check valid button

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,12 +5,15 @@
  */
 package Controller;
 
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -23,25 +26,83 @@ import javax.swing.JPanel;
 public class Controller implements ActionListener {
     
     private int BUTTONSIZE = 40;
+    // number of each row
     private int broadSize = 8;
+    // store all components in panel
     private Component[] listComponents;
     private boolean isStarted = false;
+    private int playerTurn = 1;
+    private Integer[][] buttonState = new Integer[broadSize][broadSize];
+    private int invalidMoveCount = 0;
+    private int blackCount = 2;
+    private int whiteCount = 2;
     
     private void setProperty(int i, int j, JButton btn, int state) {
         btn.setName("btn"+ i + "" + j);
         btn.setPreferredSize(new Dimension(BUTTONSIZE, BUTTONSIZE));
-        if (state == 0) { // is empty
-            btn.setBackground(new Color(168, 168, 168, 1));
-            btn.setForeground(Color.GRAY);
-        } else if (state == 1) { // is black
-            btn.setBackground(Color.BLACK);
-            btn.setForeground(Color.BLACK);
-        } else if (state == 2) { // is white
-            btn.setBackground(Color.WHITE);
-            btn.setForeground(Color.WHITE);
+        switch (state) {
+            case -1:
+                // is white = -1
+                btn.setBackground(Color.WHITE);
+                btn.setForeground(Color.WHITE);
+                break;
+            case 0:
+                // is empty = 0
+                btn.setBackground(new Color(168, 168, 168, 1));
+                btn.setForeground(new Color(168, 168, 168, 1));
+                break;
+            case 1:
+                // is black = 1
+                btn.setBackground(Color.BLACK);
+                btn.setForeground(Color.BLACK);
+                break;
+            default:
+                break;
         }
         btn.addActionListener(this);
         btn.setVisible(true);
+    }
+    
+    
+//    private void checkAllValidMove() {
+//        int count = 0;
+//        for (int i = 0; i < broadSize; i++) {
+//            for (int j = 0; j < broadSize; j++) {
+//                if (isValidMove(playerTurn, i, j)) {
+//                    buttonState[i][j] = 2;
+//                    count++;
+//                }
+//            }
+//        }
+//    }
+//    
+    private boolean isValidMove(int color, int row, int col) {
+        if (buttonState[row][col] != 0) {
+            return false;
+        }
+        
+        int dr, dc;
+        for (dr = -1; dr <= 1; dr++) {            
+            for (dc = -1; dc <= 1; dc++) {
+                if (!(dr == 0 && dc == 0) && IsOutflanking(color, row, col, dr, dc))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean IsOutflanking(int color, int row, int col, int dr, int dc)
+    {
+        int r = row + dr;
+        int c = col + dc;
+        while (r >= 0 && r < broadSize && c >= 0 && c < broadSize && buttonState[r][c] == -color)
+        {
+            r += dr;
+            c += dc;
+        }
+        if (r < 0 || r >= broadSize || c < 0 || c >= broadSize || (r - dr == row && c - dc == col) || buttonState[r][c] != color)
+            return false;
+        return true;
     }
     
     public void initialButton(JPanel myPanel) {
@@ -55,24 +116,131 @@ public class Controller implements ActionListener {
                 btn = new JButton();
                 myPanel.add(btn);
                 if (i==3 && j == 3) {
-                    setProperty(i, j, btn, 2);
+                    setProperty(i, j, btn, -1);
+                    buttonState[i][j] = -1;
+                    btn.removeActionListener(this);
                 } else
                 if (i==3 && j == 4) {
                     setProperty(i, j, btn, 1);
+                    buttonState[i][j] = 1;
+                    btn.removeActionListener(this);
                 } else
                 if (i==4 && j == 3) {
                     setProperty(i, j, btn, 1);
+                    buttonState[i][j] = 1;
+                    btn.removeActionListener(this);
                 } else
                 if (i==4 && j == 4) {
-                    setProperty(i, j, btn, 2);
+                    setProperty(i, j, btn, -1);
+                    buttonState[i][j] = -1;
+                    btn.removeActionListener(this);
                 } else {
                     setProperty(i, j, btn, 0);
+                    buttonState[i][j] = 0;
+                }
+            }
+        }
+        
+        // check all valid move
+        for (int i = 0; i < broadSize; i++) {
+            for (int j = 0; j < broadSize; j++) {
+                if (isValidMove(playerTurn, i, j)) {
+                    buttonState[i][j] = 2;
                 }
             }
         }
         listComponents = myPanel.getComponents();
         myPanel.revalidate();
+        
+        // remove action listener of invalid button
+        // can enable button = false because disable button's background not working
+        for (int i = 0; i < broadSize; i++) {
+            for (int j = 0; j < broadSize; j++) {
+                if (buttonState[i][j] == 0) {
+                    JButton b = new JButton();
+                    b = (JButton) listComponents[i * 8 + j];
+                    b.removeActionListener(this);
+                }
+            }
+        }
     }
+    
+//    void UpdateButtons(boolean isMoved)
+//    {
+//        if (isMoved)
+//        {
+//            // Update buttons' states after moving.
+//            for (int i = 0; i < broadSize; i++)
+//            {
+//                for (int j = 0; j < broadSize; j++)
+//                {
+//                    switch (buttonState[i][j])
+//                    {
+//                        case 2:
+//                            buttonState[i][j] = 0;
+//                            break;
+//                    }
+//                }
+//            }
+//        }
+//        else
+//        {
+//            // Enable canMoved buttons.
+//            for (int i = 0; i < broadSize; i++)
+//            {
+//                for (int j = 0; j < broadSize; j++)
+//                {
+//                    if (buttonState[i][j] == 2)
+//                    {
+////                        btn.addActionListener(this);
+//                    }
+//                    else
+//                    {
+////                        btn.removeActionListener(this);
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    public void MakeMove(int color, int row, int col)
+//    {
+//        // Set array's value
+//        buttonState[row][col] = color;
+//        if (playerTurn == 1)
+//        {
+//            blackCount++;
+//        }
+//        else
+//        {
+//            whiteCount++;
+//        }
+//
+//        // Flip outflanking position
+//        int dr, dc;
+//        int r, c;
+//        for (dr = -1; dr <= 1; dr++)
+//        {
+//            for (dc = -1; dc <= 1; dc++)
+//            {
+//                if (!(dr == 0 && dc == 0) && IsOutflanking(color, row, col, dr, dc))
+//                {
+//                    r = row + dr;
+//                    c = col + dc;
+//                    while (buttonState[r][c] == -color)
+//                    {
+//                        buttonState[r][c] = color;
+//                        blackCount += playerTurn;
+//                        whiteCount -= playerTurn;
+//                        r += dr;
+//                        c += dc;
+//                    }
+//                }
+//            }
+//        }
+//
+////        UpdateButtons(true);
+//    }
     
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -85,21 +253,6 @@ public class Controller implements ActionListener {
             int xClicked = Integer.parseInt(btn.getName().substring(btn.getName().length() - 2, btn.getName().length() - 1));
             int yClicked = Integer.parseInt(btn.getName().substring(btn.getName().length() - 1));
             System.out.println(xClicked + "-" + yClicked);
-            // có hàm check xem người chơi có được đánh ở ô này không trả về true false
-            // ví dụ tra ve boolean isValiable = true;
-            boolean isValiable = true;
-            if (isValiable) {
-                // xét xem đó là người chơi "Client - Rival" hay "Server - you"
-                // nếu client thì cho 1 tham số int là 2, còn là server thì là 1
-                // ví dụ đây là client
-                // xu ly o day
-                int who = 2;
-                if (who==2) {
-                    btn.setBackground(Color.WHITE);
-                } else if (who==1) {
-                    btn.setBackground(Color.BLACK);
-                }
-            }
         }
     }
     
